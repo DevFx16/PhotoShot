8b1eb92ab879d432d79034578698fa5623edbe36
“Fernando <fernandoaraujo081@gmail.com>
2019-02-26 17:53:04 -0500

    Radio

--- lib/Views/Modal.dart
+++ lib/Views/Modal.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tags/input_tags.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Modal {
   Modal(GlobalKey<ScaffoldState> _key) {
@@ -13,6 +16,70 @@ class _Modal extends StatefulWidget {
 }
 
 class View extends State<_Modal> {
+  int group = 0;
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  _estado(int e) {
+    setState(() {
+      this.group = e;
+    });
+  }
+
+  Widget _componente() {
+    if (this.group == 0) {
+      return TextField(
+        cursorColor: Colors.red,
+        decoration: InputDecoration(
+          labelText: 'Link',
+          labelStyle: TextStyle(fontSize: 16, color: Colors.red),
+          icon: Icon(Icons.link),
+        ),
+      );
+    } else if (this.group == 1) {
+      return Container(
+        alignment: Alignment(0, 0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: (MediaQuery.of(context).size.width / 2) - 50,
+              alignment: Alignment(0, 0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.red, width: 3),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: _image == null
+                    ? Icon(Icons.broken_image, size: 100, color: Colors.red)
+                    : Image.file(_image, fit: BoxFit.fill),
+              ),
+            ),
+            Container(padding: EdgeInsets.only(top: 30)),
+            RaisedButton(
+              color: Colors.red,
+              textColor: Colors.white,
+              onPressed: () async => await getImage(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.image),
+                  Text('Seleccionar', style: TextStyle(fontSize: 18))
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return null;
+  }
+
   List<String> _tags = [];
   @override
   Widget build(BuildContext context) {
@@ -40,6 +107,36 @@ class View extends State<_Modal> {
             onInsert: (value) => print(value),
             onDelete: (value) => print(value),
           ),
+          Container(
+            padding: EdgeInsets.only(top: 20),
+            child: ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Radio(
+                  value: 0,
+                  groupValue: this.group,
+                  activeColor: Colors.red,
+                  onChanged: (e) => _estado(e),
+                ),
+                Text('Link', style: TextStyle(fontSize: 16, color: Colors.red)),
+                Radio(
+                  value: 1,
+                  groupValue: this.group,
+                  activeColor: Colors.red,
+                  onChanged: (e) => _estado(e),
+                ),
+                Text(
+                  'Archivo',
+                  style: TextStyle(fontSize: 16, color: Colors.red),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: (MediaQuery.of(context).size.width / 2) + 50,
+            padding: EdgeInsets.only(top: 20),
+            child: _componente(),
+          )
         ],
       ),
     );
